df0fb816673d589ec8eed80546d5a2607e7de8f0
Eknarong Aphiphutthikul <eknarong.aph@ascendcorp.com>
2024-01-22 10:43:30 +0700

    init project demo parent and common lib

---
+++ java-kotlin/maven-parent/demo-use-common-lib-parent/src/main/kotlin/com/example/demo/demousecommonlibparent/DemoUseCommonLibParentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.demousecommonlibparent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoUseCommonLibParentApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoUseCommonLibParentApplication>(*args)
+}
---
+++ java-kotlin/maven-parent/demo-use-common-lib-parent/src/test/kotlin/com/example/demo/demousecommonlibparent/DemoUseCommonLibParentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo.demousecommonlibparent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoUseCommonLibParentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
