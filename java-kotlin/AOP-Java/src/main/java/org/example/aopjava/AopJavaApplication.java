9472b380494f6b1738f910c85c093db7a76a5c3b
Eknarong Aphiphutthikul <akenarongake@hotmail.com>
2024-04-18 11:59:24 +0700

    init spring boot project

---
+++ java-kotlin/AOP-Java/src/main/java/org/example/aopjava/AopJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.aopjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopJavaApplication.class, args);
+    }
+
+}
---
+++ java-kotlin/AOP-Java/src/test/java/org/example/aopjava/AopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aopjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
