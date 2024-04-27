1d96fe57ddb6a0bf28df3b5ffd8c44e129ed5ddf
Eknarong Aphiphutthikul <akenarongake@hotmail.com>
2024-04-27 12:05:06 +0700

    poc call method is aspect in aop

--- java-kotlin/AOP-Java/src/main/java/org/example/aopjava/aspect/ExampleAspect.java
+++ java-kotlin/AOP-Java/src/main/java/org/example/aopjava/aspect/ExampleAspect.java
@@ -2,6 +2,7 @@ package org.example.aopjava.aspect;
 
 import lombok.extern.slf4j.*;
 import org.aspectj.lang.annotation.*;
+import org.example.aopjava.service.*;
 import org.springframework.stereotype.*;
 
 @Aspect
@@ -9,13 +10,28 @@ import org.springframework.stereotype.*;
 @Component
 public class ExampleAspect {
 
-        @Before("execution(* org.example.aopjava.service.ExampleService.exampleMethod())")
-        public void beforeExampleMethod() {
-            log.info("Before exampleMethod called");
-        }
+    private final ExampleService exampleService;
 
-        @After("execution(* org.example.aopjava.service.ExampleService.exampleMethod())")
-        public void afterExampleMethod() {
-           log.info("After exampleMethod called");
-        }
+    public ExampleAspect(ExampleService exampleService) {
+        this.exampleService = exampleService;
+    }
+
+    //* error:
+    /*
+    @Before("execution(* org.example.aopjava.service.ExampleService.exampleMethod())")
+    public void beforeExampleMethodCallService() {
+        log.info("Before exampleMethod called");
+        exampleService.exampleMethod();
+    }
+     */
+
+    @Before("execution(* org.example.aopjava.service.ExampleService.exampleMethod())")
+    public void beforeExampleMethod() {
+        log.info("Before exampleMethod called");
+    }
+
+    @After("execution(* org.example.aopjava.service.ExampleService.exampleMethod())")
+    public void afterExampleMethod() {
+       log.info("After exampleMethod called");
+    }
 }
