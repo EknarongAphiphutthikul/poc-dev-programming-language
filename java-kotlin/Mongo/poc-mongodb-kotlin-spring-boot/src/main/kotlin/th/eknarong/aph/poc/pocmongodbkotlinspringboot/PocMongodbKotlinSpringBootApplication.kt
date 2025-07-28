1cf3e08b98d459fa588e4f3346f747cc131ead58
Eknarong Aphiphutthikul <akenarongake@hotmail.com>
2025-07-28 16:36:11 +0700

    create project poc-mongodb-kotlin-spring-boot

---
+++ java-kotlin/Mongo/poc-mongodb-kotlin-spring-boot/src/main/kotlin/th/eknarong/aph/poc/pocmongodbkotlinspringboot/PocMongodbKotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package th.eknarong.aph.poc.pocmongodbkotlinspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocMongodbKotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<PocMongodbKotlinSpringBootApplication>(*args)
+}
---
+++ java-kotlin/Mongo/poc-mongodb-kotlin-spring-boot/src/test/kotlin/th/eknarong/aph/poc/pocmongodbkotlinspringboot/PocMongodbKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package th.eknarong.aph.poc.pocmongodbkotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocMongodbKotlinSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
